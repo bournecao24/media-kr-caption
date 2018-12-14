9cd2dc28ba5f2e0824f1c96cf1fa330c42ce34f2
caozhenlong <caozhenlong@36kr.com>
2018-12-14 17:54:57 +0800

    Initial commit

---
+++ src/main/java/com/kr/caption/controller/TestClass.java
@@ -0,0 +1,18 @@
+package com.kr.caption.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestClass {
+
+    @RequestMapping("test")
+    @ResponseBody
+    public String test(){
+        System.out.println("hello word ");
+        return "success";
+    }
+}
