7237784d1e8b17500d69b3fae3928bfaa3a00a13
caozhenlong <caozhenlong@36kr.com>
2019-07-31 19:08:50 +0800

    检查在运行

--- src/main/java/com/kr/caption/spring/orginal/BlankDisc.java
+++ src/main/java/com/kr/caption/spring/orginal/BlankDisc.java
@@ -1,13 +1,18 @@
 package com.kr.caption.spring.orginal;
 
+import java.util.List;
+
 public class BlankDisc implements Compactdic {
 
     private String title;
     private String article;
+    private List<String> tracks;
 
-    public BlankDisc(String title, String article) {
+
+    public BlankDisc(String title, String article, List<String> tracks) {
         this.title = title;
         this.article = article;
+        this.tracks = tracks;
     }
 
     @Override
