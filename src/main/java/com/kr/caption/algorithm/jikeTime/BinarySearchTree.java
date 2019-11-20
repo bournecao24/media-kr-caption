b6a0505f3e4a2098f2389fca371e716e3e45a994
zhenlongla0824@163.com <losangeles24@>
2019-11-21 00:02:54 +0800

    数据结构和算法

---
+++ src/main/java/com/kr/caption/algorithm/jikeTime/BinarySearchTree.java
@@ -0,0 +1,52 @@
+package com.kr.caption.algorithm.jikeTime;
+
+public class BinarySearchTree {
+
+    private Node tree;
+
+    public Node find(int data) {
+        Node p = tree;
+        while (p != null) {
+            if (data < p.data) p = p.left;
+            else if (data > p.data) p = p.right;
+            else return p;
+        }
+        return null;
+    }
+
+
+    public void insert(int data) {
+        if (tree == null) {
+            tree = new Node(data);
+            return;
+        }
+
+        Node p = tree;
+        while (p != null) {
+            if (data > p.data) {
+                if (p.right == null) {
+                    p.right = new Node(data);
+                    return;
+                }
+                p = p.right;
+            } else { // data < p.data
+                if (p.left == null) {
+                    p.left = new Node(data);
+                    return;
+                }
+                p = p.left;
+            }
+        }
+    }
+
+
+    public static class Node {
+        private int data;
+        private Node left;
+        private Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+}
