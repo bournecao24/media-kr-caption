33af5fb548620c75c9029a44b56865fb2bd362f6
caozhenlong <caozhenlong@36kr.com>
2019-12-25 18:11:01 +0800

    集合分批处理数据

--- src/main/java/com/kr/caption/algorithm/Test.java
+++ src/main/java/com/kr/caption/algorithm/Test.java
@@ -1,4 +1,31 @@
 package com.kr.caption.algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
+
+
+    private static final Integer LIMIT_NUM = 20;
+
+    private void subListSkill() {
+
+        List<String> list = new ArrayList<>(100);
+
+        if (list.size() > LIMIT_NUM) {
+            int part = list.size() / LIMIT_NUM;
+            for (int i = 0; i < part; i++) {
+                List<String> taskBatchList = list.subList(0, LIMIT_NUM);
+                //to do something
+                list.subList(0, LIMIT_NUM).clear();
+            }
+        }
+
+        if (list.size() > 0) {
+            //to do something
+            list.clear();
+        }
+    }
+
+
 }
