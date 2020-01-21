d6163f3db306164169051933ff7402fbce0161ee
caozhenlong <caozhenlong@36kr.com>
2020-01-21 11:49:50 +0800

    Spring

---
+++ src/main/java/com/kr/caption/TestClassV2.java
@@ -0,0 +1,18 @@
+package com.kr.caption;
+
+import com.kr.caption.spring.aspect.AudienceV4;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+public class TestClassV2 {
+
+    public static void main(String[] args) {
+
+        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("application_example.xml"));
+
+        AudienceV4 audience =(AudienceV4) xmlBeanFactory.getBean("audience");
+        System.out.println(audience);
+
+    }
+}
